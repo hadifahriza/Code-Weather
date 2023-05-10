f8cc270121fd15765e4d2dd4fbb8807586f8eb8a
hadi.fahriza <hadi.fahriza@caditglobal.com>
2023-05-11 01:41:35 +0700

    remove secret key

--- lib/core/constants/api_url.dart
+++ lib/core/constants/api_url.dart
@@ -1,7 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiUrl {
   const ApiUrl._();
 
-  static const String baseUrl = 'https://api.openweathermap.org/data/2.5/onecall';
-
-  static const String appid = '9a33355b0c150f958639ea72fa2517ab';
+  static String baseUrl = dotenv.get('BASE_URL', fallback: '-');
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app.dart';
 import 'injection.dart' as di;
 
-void main() {
+void main() async {
   di.init();
+  await dotenv.load(fileName: ".env");
   runApp(const App());
 }
