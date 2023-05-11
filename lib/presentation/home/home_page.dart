a838be8ac0301c5ea9ebc62f912b0ad070a8dfd8
hadi.fahriza <hadi.fahriza@caditglobal.com>
2023-05-11 16:45:13 +0700

    fix homepage

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -35,89 +35,90 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        shadowColor: Colors.transparent,
-        automaticallyImplyLeading: false,
-        centerTitle: false,
-        title: InkWell(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage()));
-          },
-          child: Row(
-            children: [
-              Icon(
-                Icons.search,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          title: Text(
+            'Weather in your location',
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          // title: TextField(
+          //   decoration: InputDecoration(
+          //     border: OutlineInputBorder(
+          //       borderRadius: BorderRadius.circular(ConstantStyle.radius10),
+          //       borderSide: BorderSide.none,
+          //     ),
+          //     filled: true,
+          //     fillColor: Theme.of(context).colorScheme.secondary.withOpacity(ConstantStyle.opacity25),
+          //     contentPadding: const EdgeInsets.symmetric(horizontal: ConstantStyle.padding10),
+          //     prefixIcon: Icon(
+          //       Icons.search,
+          //       color: Theme.of(context).colorScheme.onBackground,
+          //     ),
+          //   ),
+          // ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingPage()));
+              },
+              icon: Icon(
+                Icons.settings,
                 color: Theme.of(context).colorScheme.onBackground,
               ),
-              const SizedBox(width: ConstantStyle.width10),
-              Text(
-                'Lokasi',
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingPage()));
-            },
-            icon: Icon(
-              Icons.settings,
-              color: Theme.of(context).colorScheme.onBackground,
             ),
-          ),
-        ],
-      ),
-      body: RefreshIndicator(
-        onRefresh: () async => fetchData(),
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: ConstantStyle.padding10, vertical: ConstantStyle.padding10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Temperature(data: context.read<WeatherBloc>().state.generalWeather?.current),
-              // MaterialButton(
-              //   onPressed: () {
-              //     // TODO: change to autoRoute
-              //     Navigator.push(context, MaterialPageRoute(builder: (context) => const DetailsPage()));
-              //   },
-              //   color: Theme.of(context).colorScheme.primary,
-              //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(ConstantStyle.radius10)),
-              //   child: Row(
-              //     mainAxisSize: MainAxisSize.min,
-              //     children: [
-              //       Icon(
-              //         Icons.info,
-              //         color: Theme.of(context).colorScheme.onPrimary,
-              //       ),
-              //       const SizedBox(width: ConstantStyle.width10),
-              //       Text(
-              //         'Waspada Hujan Lebat - Moderate',
-              //         style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              //               color: Theme.of(context).colorScheme.onPrimary,
-              //             ),
-              //       ),
-              //     ],
-              //   ),
-              // ),
-              const SizedBox(height: ConstantStyle.height20),
-              WeatherBox(data: context.read<WeatherBloc>().state.generalWeather?.current),
-              const SizedBox(height: ConstantStyle.height20),
-              HourList(data: context.read<WeatherBloc>().state.generalWeather?.hourly),
-              const SizedBox(height: ConstantStyle.height20),
-              isList
-                  ? DateList(onTap: (value) => changeList)
-                  : DetailList(
-                      onBack: changeList,
-                      data: context.read<WeatherBloc>().state.generalWeather?.daily,
-                    ),
-            ],
+          ],
+        ),
+        body: RefreshIndicator(
+          onRefresh: () async => fetchData(),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: ConstantStyle.padding10, vertical: ConstantStyle.padding10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Temperature(data: context.read<WeatherBloc>().state.generalWeather?.current),
+                // MaterialButton(
+                //   onPressed: () {
+                //     Navigator.push(context, MaterialPageRoute(builder: (context) => const DetailsPage()));
+                //   },
+                //   color: Theme.of(context).colorScheme.primary,
+                //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(ConstantStyle.radius10)),
+                //   child: Row(
+                //     mainAxisSize: MainAxisSize.min,
+                //     children: [
+                //       Icon(
+                //         Icons.info,
+                //         color: Theme.of(context).colorScheme.onPrimary,
+                //       ),
+                //       const SizedBox(width: ConstantStyle.width10),
+                //       Text(
+                //         'Waspada Hujan Lebat - Moderate',
+                //         style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                //               color: Theme.of(context).colorScheme.onPrimary,
+                //             ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
+                const SizedBox(height: ConstantStyle.height20),
+                WeatherBox(data: context.read<WeatherBloc>().state.generalWeather?.current),
+                const SizedBox(height: ConstantStyle.height20),
+                HourList(data: context.read<WeatherBloc>().state.generalWeather?.hourly),
+                const SizedBox(height: ConstantStyle.height20),
+                isList
+                    ? DateList(onTap: (value) => changeList)
+                    : DetailList(
+                        onBack: changeList,
+                        data: context.read<WeatherBloc>().state.generalWeather?.daily,
+                      ),
+              ],
+            ),
           ),
         ),
       ),
